26ca4fbba8e52534578296b0ff8e876a705cd7c4
ugudmundsson <u.gudmundsson@gmail.com>
2025-05-14 10:24:25 +0200

    Add project files.

---
+++ WebAPI/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data;
+
+public class DataContext(DbContextOptions<DataContext> options) : IdentityDbContext(options)
+{
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
+}
